19d5f80615a16cff3323973477cfa2d62d5a7ff7
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-08 12:17:32 +0500

    Completed All The Coding And Saved The Voice In A File

--- Text To Speech.py
+++ Text To Speech.py
@@ -4,8 +4,19 @@ import pyttsx3
 # Initializing A Function For Getting An Engine Instance For The Speech
 engine = pyttsx3.init()
 
+# Getting And Setting Speed Of The Voice
+engine.getProperty('rate')
+engine.setProperty('rate',120)
+
+# Writing Phrase To Be Spoken
+string = "Hello Sir! How Are You?"
+
 # Passing Text To Be Spoken In The Say Method On Engine
-engine.say("Hello World")
+engine.say(string)
+
+# Saving Voice In A File
+engine.save_to_file(string, 'voice.mp3')
 
 # Run And Wait Method To Process The Voice Commands
 engine.runAndWait()
+
