d408b4d3b3c12504f0c57c670e6139728af73928
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-04-08 11:35:06 +0500

    Passed Text In The Say Method

--- Text To Speech.py
+++ Text To Speech.py
@@ -4,3 +4,8 @@ import pyttsx3
 # Initializing A Function For Getting An Engine Instance For The Speech
 engine = pyttsx3.init()
 
+# Passing Text To Be Spoken In The Say Method On Engine
+engine.say("Hello World")
+
+# Run And Wait Method To Process The Voice Commands
+engine.runAndWait()
